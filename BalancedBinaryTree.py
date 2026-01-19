a4a7f4560187bcefd9cfd6870c5d7620f361634e
DiegoSer23 <50917522+DiegoSer23@users.noreply.github.com>
2026-01-19 16:29:00 -0600

    Implement balanced binary tree check

---
+++ BalancedBinaryTree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getHeight(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        leftHeight = self.getHeight(root.left)
+        if leftHeight == -1:
+            return -1
+        rightHeight = self.getHeight(root.right)
+        if rightHeight == -1:
+            return -1
+        if abs(leftHeight - rightHeight) > 1:
+            return -1
+        return 1 + max(leftHeight, rightHeight)
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return self.getHeight(root) != -1
