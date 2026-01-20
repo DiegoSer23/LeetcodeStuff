ad75763f4eb4b1698a2895edc090f5b3d469b51a
DiegoSer23 <50917522+DiegoSer23@users.noreply.github.com>
2026-01-20 13:19:15 -0600

    Implement maxProfit method to calculate stock profit

---
+++ TimeBuyStocks.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        buy = 0
+        for i in range(1, len(prices)):
+            if prices[buy] >= prices[i]:
+                buy = i
+            else:
+                if prices[i] - prices[buy] > profit:
+                    profit = prices[i] - prices[buy]
+        return profit
